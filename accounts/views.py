2f2084f8c3f7af260ed0a927ce702cd8010db0fc
Amir Cheshmberah <amirm.cheshmberah@gmail.com>
2025-09-15 22:11:30 +0330

    correct some misspelling

--- accounts/views.py
+++ accounts/views.py
@@ -5,4 +5,4 @@ from .forms import CustomUserCreationForm, CustomUserChangeForm
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('home')
-    template_name = 'registartion/signup.html'
+    template_name = 'registration/signup.html'
