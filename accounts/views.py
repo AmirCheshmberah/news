5caa7c688a7a27c2d2c96585ee9103326e179f34
Amir Cheshmberah <amirm.cheshmberah@gmail.com>
2025-09-15 21:48:02 +0330

    add signupview class

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 
-def new_user_view(request):
-    if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
-    else:
-        form = CustomUserCreationForm()
-    return render(request, 'new_user_form.html', {'form':form})
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('home')
+    template_name = 'registartion/signup.html'
