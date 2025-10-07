01d8276f787576cf7a19ad18d138d87ada0262ef
Amir Cheshmberah <amirm.cheshmberah@gmail.com>
2025-10-07 22:47:47 +0330

    show comments in templates

--- articles/admin.py
+++ articles/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from . import models
 
+
+class CommentInline(admin.StackedInline):
+    model = models.Comment
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [CommentInline,]
     list_display = ['title', 'body', 'author']
 
 
 admin.site.register(models.Article, ArticleAdmin)
+admin.site.register(models.Comment)
